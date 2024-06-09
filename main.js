9ddf5a6ece4cc1c9b26b29aec7f062661f626dc7
emilianoduque <emiyaeldi85@gmail.com>
2024-06-09 15:36:00 -0300

    TOP Etch-a-Sketch

---
+++ main.js
@@ -0,0 +1,52 @@
+let color = "black";
+
+document.addEventListener("DOMContentLoaded", function(){
+ createBoard(16);
+
+ let btn_popup = document.getElementById("popup");
+ btn_popup.addEventListener("click", function(){
+    let size = getSize();
+    createBoard(size);
+ })
+})
+
+function createBoard(size){
+  let board = document.querySelector(".board");
+
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++){
+    let div = document.createElement("div");
+    div.addEventListener("mouseover", colorDiv);
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
+
+function getSize(){
+    let input = parseInt(prompt("Put the size here"));
+    let message = document.getElementById("message");
+    if(input == ""){
+      message.innerText = "Provide a valid number"
+    }else if (input < 0 || input > 100 || isNaN(input)){
+      message.innerText = "Provide a number between 1 and 100"
+    }else {
+      message.innerText = "Now you can play!"
+    }
+    return input;
+}
+
+function colorDiv(){
+  if(color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
+  } else {
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice){
+  color = colorChoice;
+};
+

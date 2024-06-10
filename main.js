f8b1eb2f04270283d07dbdbf16fd3cab12b37a02
emilianoduque <emiyaeldi85@gmail.com>
2024-06-09 22:39:40 -0300

    new changes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let color = "black";
 
-document.addEventListener("DOMContentLoaded", function(){
- createBoard(16);
+document.addEventListener("DOMContentLoaded", function () {
+  createBoard(16);
 
- let btn_popup = document.getElementById("popup");
- btn_popup.addEventListener("click", function(){
+  let btn_popup = document.getElementById("popup");
+  btn_popup.addEventListener("click", function () {
     let size = getSize();
     createBoard(size);
- })
-})
+  });
+});
 
-function createBoard(size){
+function createBoard(size) {
   let board = document.querySelector(".board");
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -18,35 +18,39 @@ function createBoard(size){
 
   let numDivs = size * size;
 
-  for (let i = 0; i < numDivs; i++){
+  for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
     div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
 }
 
-function getSize(){
-    let input = parseInt(prompt("Put the size here"));
-    let message = document.getElementById("message");
-    if(input == ""){
-      message.innerText = "Provide a valid number"
-    }else if (input < 0 || input > 100 || isNaN(input)){
-      message.innerText = "Provide a number between 1 and 100"
-    }else {
-      message.innerText = "Now you can play!"
-    }
-    return input;
+function getSize() {
+  let input = parseInt(prompt("Put the size here"));
+  let message = document.getElementById("message");
+  if (input == "") {
+    message.innerText = "Provide a valid number";
+  } else if (input < 0 || input > 100 || isNaN(input)) {
+    message.innerText = "Provide a number between 1 and 100";
+  } else {
+    message.innerText = "Now you can play!";
+  }
+  return input;
 }
 
-function colorDiv(){
-  if(color == "random"){
+function colorDiv() {
+  if (color == "random") {
     this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%`;
   } else {
-    this.style.backgroundColor = 'black';
+    this.style.backgroundColor = "black";
   }
 }
 
-function setColor(colorChoice){
+function setColor(colorChoice) {
   color = colorChoice;
-};
+}
 
+function resetBoard() {
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
+}
